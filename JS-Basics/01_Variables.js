394072a78793e0d6ab792a7c7d9cc785a9b73878
Sandeepp6860 <115526518+Sandeepp6860@users.noreply.github.com>
2023-12-09 19:05:07 +0000

    Understand Variables in JavaScript

---
+++ JS-Basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId=1224325; // Its value cannot be changed. When a variable is declared with const, JS will not allow to change its value.
+let accountEmail="Sandeep@gmail.com";
+var firstName="Sandeep";
+
+accountCity="Hyderabad";  // Variable cannot be defined in this way. It is a bad Practice.
+
+console.table([accountId,accountEmail,firstName,accountCity]);
