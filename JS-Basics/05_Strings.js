6e973de9d3b6089df12c5dba9d65b21b681bc2b0
Sandeepp6860 <115526518+Sandeepp6860@users.noreply.github.com>
2023-12-10 16:12:10 +0000

    Understand Strings in JS

---
+++ JS-Basics/05_Strings.js
@@ -0,0 +1,11 @@
+const firstName=new String("SandeepPodduturi");
+console.log(firstName);
+
+console.log(firstName.slice(0,4));
+
+const lastName="  Sandeep     ";
+console.log(lastName.trim());
+
+const anotherString="https://sandeep.com/sandeep%Podduturi";
+
+console.log(anotherString.replace("%","-"));
