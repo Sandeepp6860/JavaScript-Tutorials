101b3c6335221ff3f851580931f313be6591a37f
Sandeepp6860 <115526518+Sandeepp6860@users.noreply.github.com>
2023-12-10 14:11:49 +0000

    Prefix and Postfix increment Operator

--- JS-Basics/03_ValuesCoversion.js
+++ JS-Basics/03_ValuesCoversion.js
@@ -36,9 +36,25 @@ console.log(typeof scoreInNumber);
 
 // null => 0
 
-let isLoggedIn=1;
+// let isLoggedIn=1;
 
-let booleanIsLoggedIn=Boolean(isLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//Postfix
+
+let x=10;
+let y=x++; // When Postfix operator is used, the increment operator increments the value but returns the value before increment.
+        // Here x value will be 4, but value of x is 3 before increment. so the value of y will be 3.
+
+console.log(`X: ${x},Y: ${y}`);
+
+// Prefix Operator:
+
+let a=10;
+let b=++a; // When Prefix is used, the increment operator increments the value and returns the value after incrementing.
+        // Here a value will be 4 and b value will be 4. b value will be 4 because prefix increment operator returns the value after incrementing.
+
+
 
