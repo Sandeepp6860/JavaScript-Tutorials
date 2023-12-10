5e80e53814f721dbb16425b93459e49b42664a9f
Sandeepp6860 <115526518+Sandeepp6860@users.noreply.github.com>
2023-12-10 14:27:32 +0000

    Understand Comparision Operators

---
+++ JS-Basics/04_ComparisionOperators.js
@@ -0,0 +1,9 @@
+console.log(2=="2"); // Normal comparision, does not check data types.
+console.log(2==="2"); // Strict comparision ,checks datatypes and values.
+
+// Comparision Operators
+
+// <,>,<=,>=, !=,!==
+
+
+console.log("2">1);
