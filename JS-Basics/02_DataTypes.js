f49c89b06153baa4dfbd66fb96fc159bb231bdb9
Sandeepp6860 <115526518+Sandeepp6860@users.noreply.github.com>
2023-12-10 13:14:01 +0000

    Understand Data Types in JS

---
+++ JS-Basics/02_DataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert("Hello?");// We are using nodejs not browser.
+
+// DataTypes:
+
+//1. Numbers
+
+const int=45;
+console.log("Integer:",int);
+
+const floatNumber=3435.785;
+console.log("Float:",floatNumber);
+
+//2. Strings
+
+const firstName="Sandeep";
+console.log("First Name:",firstName);
+
+// 3. Boolean 
+
+const isLoggedIn=true;
+
+console.log("Is Logged In:",isLoggedIn);
+
+//4. null
+
+//5. Undefined:
+
+let lastName;
+console.log(lastName);
+
+//6. Symbol => Unique
+
+//7. Objects
+
+
+console.log(typeof undefined);
+console.log(typeof null);
